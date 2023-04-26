b989b3eaca9832d98618b1877b4a723eb55c5275
Chriistinaa <delefosse.christina62@gmail.com>
2023-04-26 22:44:52 +0200

    init

---
+++ js/button.js
@@ -0,0 +1,35 @@
+function btn() {
+    var btn = document.getElementById("btn");
+    if (btn.innerHTML === "Contact") {
+        btn.innerHTML = "Au 00 65 87 99 00";
+    } else {
+        btn.innerHTML = "Contact";
+    }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
